5da3f5ed224a57b4d9c0d95f10233860db0b7ac5
Dulanga Niroshan <dulanganiroshan721@gmail.com>
2024-05-06 04:28:55 +0530

    Error fixed

--- player.py
+++ player.py
@@ -1,13 +1,12 @@
 import tkinter as tk
 from tkinter import filedialog
 import pygame
-import threading
 
 class MP3Player:
     def __init__(self, master):
         self.master = master
-        self.master.title("Simple MP3 Player")
-        self.master.geometry("400x150")
+        self.master.title("D - Player")
+        self.master.geometry("300x100")
         
         self.song_label = tk.Label(self.master, text="No song selected")
         self.song_label.pack()
@@ -21,16 +20,8 @@ class MP3Player:
         self.stop_button = tk.Button(self.master, text="Stop", command=self.stop_song)
         self.stop_button.pack()
         
-        self.slider = tk.Scale(self.master, from_=0, to=100, orient="horizontal", command=self.set_volume)
-        self.slider.pack()
-        
         self.song = None
-        self.playing = False
-        self.volume = 0.5
-        self.slider.set(50)
         
-        self.update_slider_thread = None
-
     def select_song(self):
         self.song = filedialog.askopenfilename(filetypes=[("MP3 files", "*.mp3")])
         if self.song:
@@ -40,31 +31,10 @@ class MP3Player:
         if self.song:
             pygame.mixer.init()
             pygame.mixer.music.load(self.song)
-            pygame.mixer.music.set_volume(self.volume)
             pygame.mixer.music.play()
-            self.playing = True
-            self.update_slider_thread = threading.Thread(target=self.update_slider)
-            self.update_slider_thread.start()
         
     def stop_song(self):
         pygame.mixer.music.stop()
-        self.playing = False
-        if self.update_slider_thread:
-            self.update_slider_thread.join()
-        
-    def set_volume(self, value):
-        self.volume = float(value) / 100
-        if self.playing:
-            pygame.mixer.music.set_volume(self.volume)
-        
-    def update_slider(self):
-        while pygame.mixer.music.get_busy() and self.playing:
-            position = pygame.mixer.music.get_pos() / 1000  # Convert milliseconds to seconds
-            duration = pygame.mixer.Sound(self.song).get_length()
-            percentage = (position / duration) * 100
-            self.slider.set(percentage)
-            self.master.update()
-            pygame.time.wait(1000)  # Update every second
 
 def main():
     root = tk.Tk()
